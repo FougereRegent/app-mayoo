858386d04e9d177d174dfe089ea03d2a372cf8f6
damien venant-valéry <damien.venant@outlook.com>
2023-07-06 13:39:13 +0200

    Ajout ensemble des composants dans l'App

--- App.tsx
+++ App.tsx
@@ -1,9 +1,14 @@
 import { Alert, StyleSheet, Text, View } from 'react-native';
 import OwnButton from './src/components/ButtonComponents';
+import Logo from './src/components/LogoComponents';
+import Textbox, { TypeTextBox } from './src/components/TextboxComponents';
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <Logo />
+      <Textbox mode={TypeTextBox.CREDENTIALS} />
+      <Textbox mode={TypeTextBox.PASSWORD} />
       <OwnButton action={() => { Alert.alert("Hey Tous le monde") }} name='connexion' />
     </View>
   );
@@ -12,7 +17,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'red',
     alignItems: 'center',
     justifyContent: 'center',
   },
