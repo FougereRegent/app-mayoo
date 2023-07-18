2911881e4d3f23c63fd5b43ea3f9317f87c5217a
damien venant-valéry <damien.venant@outlook.com>
2023-07-18 22:17:13 +0200

    Commencement de l'écran

--- src/screens/AllergeneScreen.tsx
+++ src/screens/AllergeneScreen.tsx
@@ -1,5 +1,8 @@
 import React from "react";
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
+import Logo from "../components/LogoComponents";
+import { Title, TitleType, TextBox } from "../components/TextComponents";
+import OwnButton from "../components/ButtonComponents";
 
 
 
@@ -7,13 +10,49 @@ const AllergeneScreen = ({ navigation }: any) => {
 
   return (
     <View style={styles.container}>
+      <View style={styles.logoView}>
+        <Logo />
+        <View style={styles.headerTitle}>
+          <Title TypeTitle={TitleType.H1}>
+            Etape 1
+          </Title>
+        </View>
+        <View style={styles.headerText}>
+          <TextBox>
+            Veuillez Sélectionner les allèrgenes qui vous concernent pour que nous puissions prendre en compte
+            vos restrictions alimentaire
+          </TextBox>
+        </View>
+      </View>
+      <View style={styles.allergeneView}>
+        <OwnButton name="Suivant" action={() => { }} />
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-
+    flex: 1,
+    backgroundColor: "red"
+  },
+  logoView: {
+    flex: 1,
+    justifyContent: "center",
+    marginTop: 5
+  },
+  allergeneView: {
+    flex: 2,
+    alignItems: "center",
+  },
+  headerTitle: {
+  },
+  headerText: {
+    paddingHorizontal: 25,
+    marginVertical: 10
+  },
+  validationView: {
+    borderWidth: 1
   }
 });
 
