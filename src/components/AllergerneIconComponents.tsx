4c0a4b6ecaeeda5e46b9f597eb1b85105e06debf
FouegereRegent <damien.venant@outlook.com>
2023-07-20 22:46:04 +0200

    Ajout du composants pour la sélections des allergenes

--- src/components/AllergerneIconComponents.tsx
+++ src/components/AllergerneIconComponents.tsx
@@ -1,11 +1,10 @@
 import React from "react";
-import { View, Image, StyleSheet } from "react-native";
+import { View, Image, StyleSheet, Text } from "react-native";
 
 
 type propsAllergeneComponents = {
-  id: number,
   name: string,
-  path: string
+  path: number
 };
 
 const AllergeneIconComponents = (props: propsAllergeneComponents) => {
@@ -15,7 +14,12 @@ const AllergeneIconComponents = (props: propsAllergeneComponents) => {
     <View style={styles.container}>
       <Image
         style={styles.image}
-        source={require(props.path)} />
+        source={props.path} />
+      <View style={styles.containerText}>
+        <Text style={styles.text}>
+          {props.name.toUpperCase()}
+        </Text>
+      </View>
     </View>
   );
 
@@ -26,9 +30,24 @@ export default AllergeneIconComponents;
 
 const styles: any = StyleSheet.create({
   container: {
-
+    justifyContent: "space-around",
+    height: 85,
+    alignItems: "center"
   },
   image: {
-
+    width: 48,
+    height: 48
+  },
+  containerText: {
+    backgroundColor: "#2090f4",
+    width: 80,
+    padding: 5,
+    borderRadius: 20
+  },
+  text: {
+    color: "white",
+    fontSize: 9,
+    fontWeight: "bold",
+    textAlign: "center"
   }
 });
