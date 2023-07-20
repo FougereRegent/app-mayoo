b0c6f4d7a9016ef27a57b91d6d20d8001ed0eda2
FouegereRegent <damien.venant@outlook.com>
2023-07-20 23:01:52 +0200

    Ajout des icons sur l'écran de l'étape 1

--- src/screens/AllergeneScreen.tsx
+++ src/screens/AllergeneScreen.tsx
@@ -1,8 +1,9 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-import Logo from "../components/LogoComponents";
-import { Title, TitleType, TextBox } from "../components/TextComponents";
+import { StyleSheet, View } from "react-native";
+import AllergeneIconComponents from "../components/AllergerneIconComponents";
 import OwnButton from "../components/ButtonComponents";
+import Logo from "../components/LogoComponents";
+import { TextBox, Title, TitleType } from "../components/TextComponents";
 
 
 
@@ -11,20 +12,31 @@ const AllergeneScreen = ({ navigation }: any) => {
   return (
     <View style={styles.container}>
       <View style={styles.logoView}>
-        <Logo />
         <View style={styles.headerTitle}>
+          <Logo />
           <Title TypeTitle={TitleType.H1}>
             Etape 1
           </Title>
-        </View>
-        <View style={styles.headerText}>
-          <TextBox>
-            Veuillez Sélectionner les allèrgenes qui vous concernent pour que nous puissions prendre en compte
-            vos restrictions alimentaire
-          </TextBox>
+          <View style={styles.headerText}>
+            <TextBox>
+              Veuillez Sélectionner les allèrgenes qui vous concernent pour que nous puissions prendre en compte
+              vos restrictions alimentaire
+            </TextBox>
+          </View>
         </View>
       </View>
       <View style={styles.allergeneView}>
+        <AllergeneIconComponents path={require("../../assets/allergenes/molusque.png")} name="molusque" />
+        <AllergeneIconComponents path={require("../../assets/allergenes/sesame.png")} name="sesame" />
+        <AllergeneIconComponents path={require("../../assets/allergenes/moutarde.png")} name="moutarde" />
+        <AllergeneIconComponents path={require("../../assets/allergenes/noix.png")} name="noix" />
+        <AllergeneIconComponents path={require("../../assets/allergenes/cereale.png")} name="cereale" />
+        <AllergeneIconComponents path={require("../../assets/allergenes/poisson.png")} name="poisson" />
+        <AllergeneIconComponents path={require("../../assets/allergenes/cacahouette.png")} name="cacahouette" />
+        <AllergeneIconComponents path={require("../../assets/allergenes/crustace.png")} name="crustace" />
+        <AllergeneIconComponents path={require("../../assets/allergenes/celeri.png")} name="celeri" />
+      </View>
+      <View style={styles.validationView}>
         <OwnButton name="Suivant" action={() => { }} />
       </View>
     </View>
@@ -34,25 +46,31 @@ const AllergeneScreen = ({ navigation }: any) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#FF6847"
+    paddingVertical: 25,
+    flexDirection: "column",
+    backgroundColor: "#FF6847",
+    alignItems: "center",
   },
   logoView: {
     flex: 1,
     justifyContent: "center",
-    marginTop: 5
   },
   allergeneView: {
-    flex: 2,
+    flex: 1,
+    flexDirection: "row",
+    flexWrap: "wrap",
     alignItems: "center",
+    justifyContent: "space-between",
+    paddingHorizontal: 10,
   },
   headerTitle: {
   },
   headerText: {
     paddingHorizontal: 25,
-    marginVertical: 10
   },
   validationView: {
-    borderWidth: 1
+    flex: 1,
+    paddingTop: 25
   }
 });
 
